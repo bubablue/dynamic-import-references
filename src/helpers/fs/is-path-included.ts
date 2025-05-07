50b56e158b3f2af4979fff936052c8fda9a89b68
Jose <bubablue00@gmail.com>
2025-05-07 21:05:42 +0200

    update: split functions

--- src/helpers/fs/is-path-included.ts
+++ src/helpers/fs/is-path-included.ts
@@ -5,20 +5,18 @@ import {
   relative_dir_regex,
 } from "../regexp";
 
+const normalizeAndClean = (input: string) => {
+  const extension = path.normalize(input).replace(ext_pattern_regex, "");
+  const relativeDir = extension.replace(relative_dir_regex, "");
+  const basename = path.basename(relativeDir);
+  const dirname = path.dirname(relativeDir);
+
+  const normalized = index_pattern_regex.test(basename) ? dirname : relativeDir;
+
+  return normalized.split(path.sep).filter(Boolean);
+};
+
 export const isIncluded = (target: string, base: string): boolean => {
-  const normalizeAndClean = (input: string) => {
-    const extension = path.normalize(input).replace(ext_pattern_regex, "");
-    const relativeDir = extension.replace(relative_dir_regex, "");
-    const basename = path.basename(relativeDir);
-    const dirname = path.dirname(relativeDir);
-
-    const normalized = index_pattern_regex.test(basename)
-      ? dirname
-      : relativeDir;
-
-    return normalized.split(path.sep).filter(Boolean);
-  };
-
   const targetParts = normalizeAndClean(target);
   const baseParts = normalizeAndClean(base);
 
